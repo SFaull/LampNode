f5636c402f83b4089bcba19d4131e6170d08ad74
Sam Faull <samz5991@hotmail.co.uk>
2017-09-23 15:59:18 +0100

    Added code skeleton for final project "LampNode"

---
+++ LampNode/LampNode.ino
@@ -0,0 +1,25 @@
+/* 
+ * Author: Sam Faull
+ * Details: WiFi enabled lamp
+ *          ESP8266 WiFi module & WS281B RGB LEDs 
+ * 
+ * Pin allocations: 
+ * NA
+ */
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("Ready");
+}
+ 
+void loop() 
+{
+ // Read input
+ // Read LED control
+ 
+ // if 15 seconds has passed
+  // evaluate if button pressed and update thingspeak
+ 
+ // LED output    
+}
