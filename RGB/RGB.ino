c5a06bfbb318a99434b3eea83cb622a543506f2c
Sam Faull <samz5991@hotmail.co.uk>
2017-09-28 21:12:28 +0100

    Added 'strobing ring up and down the tube' effect

--- RGB/RGB.ino
+++ RGB/RGB.ino
@@ -9,6 +9,9 @@
 //
 
 #include <NeoPixelBrightnessBus.h> // instead of NeoPixelBus.h
+#define WIDTH   7
+#define HEIGHT  9
+
 
 const uint16_t PixelCount = 60; // this example assumes 3 pixels, making it smaller will cause a failure
 const uint8_t PixelPin = 12;  // make sure to set this to the correct pin, ignored for Esp8266
@@ -19,6 +22,21 @@ RgbColor green(0, colorSaturation, 0);
 RgbColor blue(0, 0, colorSaturation);
 RgbColor off(0,0,0);
 RgbColor white(colorSaturation, colorSaturation, colorSaturation);
+
+int index[HEIGHT][WIDTH] =  
+{
+  {0,  1,  2,  3,  4,  5,  6  },
+  {7,  8,  9,  10, 11, 12, 13 },
+  {14, 15, 16, 17, 18, 19, 20 },
+  {21, 22, 23, 24, 25, 26, 27 },
+  {28, 29, 30, 31, 32, 33, 34 },
+  {35, 36, 37, 38, 39, 40, 41 },
+  {42, 43, 44, 45, 46, 47, 48 },
+  {49, 50, 51, 52, 53, 54, 55 },
+  {56, 57, 58, 59, 60, 61, 62 } // last two not used
+};
+
+int y = 0;
 
 // Make sure to provide the correct color order feature
 // for your NeoPixels
@@ -48,20 +66,12 @@ void setup()
     
     Serial.println();
     Serial.println("Running...");
-
-    // set our three original colors
-    for (int i=0; i<PixelCount; i+=3)
-    {
-      strip.SetPixelColor(0+i, red);
-      strip.SetPixelColor(1+i, green);
-      strip.SetPixelColor(2+i, blue);
-    }
-    strip.Show();
 }
 
 
 void loop()
 {
+  /*
     uint8_t brightness = strip.GetBrightness();
     Serial.println(brightness);
     
@@ -80,8 +90,41 @@ void loop()
     // apply dimming
     brightness += direction;
     strip.SetBrightness(brightness);
-
+*/
     // show the results
+    setPosition(); 
+    //strip.Show();
+    delay(50);
+    blank();
+    //delay(5);
+    
+    if (y < HEIGHT)
+     y++;
+    else
+     y = 0;
+}
+
+
+void setPosition(void)
+{
+  Serial.print(y);
+  Serial.println(" ON");
+    // set our three original colors
+    for (int x=0; x<WIDTH; x++)
+    {
+      strip.SetPixelColor(index[y][x], green);
+    }
     strip.Show();
 }
 
+void blank(void)
+{
+  Serial.print(y-1);
+  Serial.println(" OFF");
+    // set our three original colors
+    for (int x=0; x<WIDTH; x++)
+    {
+      strip.SetPixelColor(index[y-1][x], off);
+    }
+    strip.Show();
+}
