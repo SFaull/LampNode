bf58aad7c6cec03a438d7baf11491d669c1858f5
Sam Faull <samz5991@hotmail.co.uk>
2017-12-06 20:03:01 +0000

    Removed comment at start of code and added functionality to turn light on and off by a short touch button press (untested)

--- LampNode/LampNode.ino
+++ LampNode/LampNode.ino
@@ -6,31 +6,6 @@
  * Pin allocations: 
  * NA
  */
- 
-/*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" whenever the button is pressed
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
-*/
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
@@ -83,6 +58,7 @@ char msg[50];        // message buffer
 // Flags
 bool button_pressed = false; // true if a button press has been registered
 bool button_released = false; // true if a button release has been registered
+bool button_short_press = false;
 bool target_met = false;
 bool pulse_animation = false;
 bool pulse_direction = 1;
@@ -104,9 +80,6 @@ enum Transitions {FADE, INSTANT};  // The way in which the lamp animates between
 
 enum Modes Mode;
 enum Transitions Transition;
-
-uint8_t x, 
-        y = 0;
 
 uint8_t LEDindex[HEIGHT][WIDTH] =  
 {
@@ -269,8 +242,14 @@ void loop()
       //start conting
       lastPushed = now; // start the timer 
       Serial.print("Button pushed... ");
-      client.publish("/LampNode/Comms", "Press");
       button_pressed = false;
+    }
+
+    if (((now - lastPushed) > 1000) && button_short_press) //check the hold time
+    {
+      Serial.print("Button held...");
+      client.publish("/LampNode/Comms", "Press");
+      button_short_press = false
     }
     
     if (button_released)
@@ -278,13 +257,26 @@ void loop()
       Serial.println("Button released.");
 
       //get the time that button was held in
-      buttonTime = now - lastPushed;
+      //buttonTime = now - lastPushed;
 
-      snprintf (msg, 75, "hello world #%ld", buttonTime);
-      Serial.print("Publish message: ");
-      Serial.println(msg);
-      client.publish("/test/outTopic", msg);
-      client.publish("/LampNode/Comms", "Release");
+      if (button_short_press)  // for a short press we turn the device on/off
+      {
+        if (Mode == OFF)
+          setMode(NORMAL);
+        else
+          setMode(OFF);
+
+        button_short_press = false;
+      }
+      else  // for a long press we do the animation thing
+      {
+        client.publish("/LampNode/Comms", "Release");
+      }
+
+      //snprintf (msg, 75, "hello world #%ld", buttonTime);
+      //Serial.print("Publish message: ");
+      //Serial.println(msg);
+      //client.publish("/test/outTopic", msg);
       button_released = false;
     }
   }
@@ -456,7 +448,10 @@ void readInputs(void)
   button_state = digitalRead(BUTTON); // read button state (active high)
   
   if (button_state && !last_button_state) // on a rising edge we register a button press
+  {
     button_pressed = true;
+    button_short_press = true;  // initially assume its gonna be a short press
+  }
     
   if (!button_state && last_button_state) // on a falling edge we register a button press
     button_released = true;
